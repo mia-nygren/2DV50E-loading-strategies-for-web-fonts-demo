4768aa5ad964b58dea0f0724081f17d026411d79
Mia Nygren <marianygren88@gmail.com>
2018-08-17 18:18:39 +0200

    Created an object for the merriweather font in a separate module

---
+++ app/constants/font.js
@@ -0,0 +1,15 @@
+
+// Filenames for all woff2 files
+
+const fileType = 'woff2'
+
+const merriweather = {
+  name:'merriweather',
+  regular: `merriweather-v19-latin-regular.${fileType}`,
+  bold: `merriweather-v19-latin-700.${fileType}`,
+  italic: `merriweather-v19-latin-italic.${fileType}`,
+  regularSubset: `merriweather-v19-latin-regular-subset.${fileType}`,
+  // regularSubset: `merriweather-v19-latin-italic. ${fileType}`,   TOTO subset italic and bold?
+}
+
+module.exports = merriweather;
