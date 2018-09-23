fe0f74537c8c5d62a5488106c22ed1946dc14d98
Mia Nygren <marianygren88@gmail.com>
2018-09-23 23:12:37 +0200

    added path and cleaned up push functions

--- app/utils/push.js
+++ app/utils/push.js
@@ -1,14 +1,14 @@
-const fs = require('fs')
-    // path = require('path') - TODO might need path for cleaner paths?
+const fs = require('fs'),
+    path = require('path')
 
 const pushFont = (response, fontSrc) => {
-
-    const font = fs.readFileSync('assets/' + fontSrc);
+    const fontFilePath = path.join(__dirname, '../assets/' + fontSrc);
+    const font = fs.readFileSync(fontFilePath)   //('./app/fonts/merriweather-v19-latin-regular-subset.woff2');
     // push woff2
     response.push('/' + fontSrc, {
         response: {
             'Content-Type': 'font/woff2',
-            'Cache-Control': 'no-cache' // must revalidate before using a cached copy
+            'Cache-Control': 'no-cache' // must revalidate before using a cached copy !! TODO - change this
         }
     }, (err, stream) => {
         if (err) return
@@ -19,22 +19,43 @@ const pushFont = (response, fontSrc) => {
 }
 
 
-const pushStyles = (response, file, pushPath) => {
-    // get the fs here!
-    // take in the location of the styles
+// const pushStyles = (response, file, pushPath) => {
+//     // get the fs here!
+//     // take in the location of the styles
 
-    // Note: we cached pushed files for 5 minutes (testing purposes)
-    response.push(pushPath, {    // response.push('/styles/merriweather/fonts.css'
-        response: {
-            'Content-Type': 'text/css'
-                // 'Cache-Control': 'max-age=300'
-        }
-    }, function(err, stream) {
-        if (err) {
-            return;
-        }
-        stream.end(file);
-    });
+//     // Note: we cached pushed files for 5 minutes (testing purposes)
+//     // OBS! Push path is relative to static path set in ./index.js
+//     response.push('/' + pushPath, {    // response.push('/styles/merriweather/fonts.css'
+//         response: {
+//             'Content-Type': 'text/css'
+//                 // 'Cache-Control': 'max-age=300'
+//         }
+//     }, function(err, stream) {
+//         if (err) {
+//             return;
+//         }
+//         stream.end(file);
+//     });
+// }
+
+const pushStyles = (response, filePath) => {
+  // get the fs here!
+  // take in the location of the styles
+  const styleFilePath = path.join(__dirname, '../assets/' + filePath);
+  // console.log(`styleFilePath ${styleFilePath}`);
+  const cssFile = fs.readFileSync(styleFilePath)
+  // Note: we cached pushed files for 5 minutes (testing purposes)
+  response.push('/' + filePath, {    // response.push('/styles/merriweather/fonts.css'
+      response: {
+          'Content-Type': 'text/css'
+              // 'Cache-Control': 'max-age=300'
+      }
+  }, function(err, stream) {
+      if (err) {
+          return;
+      }
+      stream.end(cssFile);
+  });
 }
 
 module.exports = {
