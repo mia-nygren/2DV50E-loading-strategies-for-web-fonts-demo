44ce111875567de99aa7b2309e26741c595ccd46
Mia Nygren <marianygren88@gmail.com>
2018-09-23 23:14:02 +0200

    pushing subset font

--- app/routes/critical-foft-push.js
+++ app/routes/critical-foft-push.js
@@ -2,35 +2,24 @@ const fs = require('fs'),
     path = require('path'),
     express = require('express'),
     router = express.Router(),
-    merriweather = require('../constants/font')
+    merriweather = require('../constants/font') // TODO - refactor or remove
 
-// get the helper method for pushing Styles
-const pushStyles = require('../utils/push').pushStyles
+// get the helper methods for pushing Styles and Fonts
+const {pushStyles, pushFont} = require('../utils/push')
 
 // critical-foft-push, use PUSH_STREAM if possible
 router.get('/critical-foft-push', function(req, res) {
 
-    const cssFontFile = req.app.get('cssFontFile')  // Works in chrome canary (initiator = push)
-
-    // pushFont(response, 'fonts/merriweather-v19-latin-regular-subset.woff2')
-    // pushFont(response, 'fonts/merriweather-v19-latin-regular.woff2')
+    pushFont(res, 'fonts/merriweather-v19-latin-regular-subset.woff2')
+    // pushFont(response, 'assets/fonts/merriweather-v19-latin-regular.woff2')
     // pushFont(response, 'fonts/merriweather-v19-latin-italic.woff2')
     // pushFont(response, 'fonts/merriweather-v19-latin-700.woff2')
     // pushFont(response, 'fonts/merriweather-v19-latin-700italic.woff2')
 
-   pushStyles(res, cssFontFile, '/styles/merriweather/fonts.css')  // works
-
+   pushStyles(res, 'styles/merriweather/fonts.css')  // works
     const indexPath = path.join(__dirname, '../public/critical-foft-push.html');
     fs.createReadStream(indexPath).pipe(res);
     return;
 });
 
 module.exports = router;
-
-//     // -- Subset font is push with link header --
-//    // If I don't have nopush in link will the asset be pushed instead of preloaded? -TESTING - TODO - push font with function instead of in Link header
-//    res.writeHead(200, {
-//     'Content-Type': 'text/html',
-//     'Cache-Control': 'no-cache', //'max-age=300'
-//     'Link':'</fonts/' + merriweather.regularSubset + '>;  rel="preload"; as="font"; crossorigin="crossorigin"; type="font/woff2";'
-// });
