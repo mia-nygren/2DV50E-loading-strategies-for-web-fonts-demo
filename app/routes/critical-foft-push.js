14c8b6c0a6a11a75d729f056d25ea28422c44d9b
Mia Nygren <marianygren88@gmail.com>
2018-09-24 23:20:25 +0200

    Changed font to Source Sans Pro

--- app/routes/critical-foft-push.js
+++ app/routes/critical-foft-push.js
@@ -1,8 +1,8 @@
 const fs = require('fs'),
     path = require('path'),
     express = require('express'),
-    router = express.Router(),
-    merriweather = require('../constants/font') // TODO - refactor or remove
+    router = express.Router() //,
+    // merriweather = require('../constants/font') // TODO - refactor or remove
 
 // get the helper methods for pushing Styles and Fonts
 const {pushStyles, pushFont} = require('../utils/push')
@@ -10,13 +10,11 @@ const {pushStyles, pushFont} = require('../utils/push')
 // critical-foft-push, use PUSH_STREAM if possible
 router.get('/critical-foft-push', function(req, res) {
 
-    pushFont(res, 'fonts/merriweather-v19-latin-regular-subset.woff2')
-    // pushFont(response, 'assets/fonts/merriweather-v19-latin-regular.woff2')
-    // pushFont(response, 'fonts/merriweather-v19-latin-italic.woff2')
-    // pushFont(response, 'fonts/merriweather-v19-latin-700.woff2')
-    // pushFont(response, 'fonts/merriweather-v19-latin-700italic.woff2')
+    // Push Source Sans Pro Regular subset
+    pushFont(res, 'fonts/source-sans-pro-latin/source-sans-pro-v11-latin-regular-subset.woff2')
 
-   pushStyles(res, 'styles/merriweather/fonts.css')  // works
+
+   pushStyles(res, 'styles/source-sans-pro-latin/fonts-4-styles-and-subset.css')
     const indexPath = path.join(__dirname, '../public/critical-foft-push.html');
     fs.createReadStream(indexPath).pipe(res);
     return;
