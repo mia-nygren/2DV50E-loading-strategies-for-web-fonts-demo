331cc868f86b9c3f8089cad942de002592391f83
Mia Nygren <marianygren88@gmail.com>
2018-09-27 02:37:18 +0200

    cleaned up response header and removed merriweather constants

--- app/routes/critical-foft-preload-header.js
+++ app/routes/critical-foft-preload-header.js
@@ -1,28 +1,24 @@
 const fs = require('fs'),
     path = require('path'),
     express = require('express'),
-    router = express.Router(),
-    merriweather = require('../constants/font')
+    router = express.Router()
 
-/* -- FONT-LOADING-STRATEGY: CRITICAL FOFT with preload in response header  --
-    Following resources are preloaded in the response link header:
-      -  regular subset woff2 file
+/*** FONT-LOADING-STRATEGY: CRITICAL FOFT with preload in response header  ***
+    The following resources are preloaded in the response link header:
+      -  regular latin subset woff2 font file
       -  css file with the @font-face blocks
 */
 
+const sourceSansProSubsetFilePath = '/fonts/source-sans-pro-latin/source-sans-pro-v11-latin-regular-subset.woff2'
+const cssFilePath = '/styles/source-sans-pro-latin/fonts-4-styles-and-subset.css'
+
 router.get('/critical-foft-preload-header', function(req, res) {
 
-    if (!req.isSpdy) { // TODO
-        return res.end('SPDY is off. We cannot use Server Push :(')
-    }
-
-   // pushStyles(res, cssFontFile, '/styles/merriweather/fonts.css')  // works
-
-    // The response head includes Link, where the regular font is preloaded
+    // The response head includes Link, where the regular font is preloaded and CSS file with @font-face blocks
     res.writeHead(200, {
         'Content-Type': 'text/html',
         'Cache-Control': 'no-cache', //'max-age=300'
-        'Link':'</fonts/' + merriweather.regularSubset + '>; rel=preload; as=font; crossorigin=crossorigin; type=font/woff2; nopush, </styles/merriweather/fonts.css>; rel=preload; as=style; nopush'
+        'Link':`<${sourceSansProSubsetFilePath}>; rel=preload; as=font; crossorigin=crossorigin; type=font/woff2; nopush, <${cssFilePath}>; rel=preload; as=style; nopush`
     });
 
     const indexPath = path.join(__dirname, '../public/critical-foft-preload-header.html');
