faa049be8c5c2db25544a7078dfb30d8ca87f2b2
Mia Nygren <marianygren88@gmail.com>
2018-09-27 02:38:45 +0200

    removed semicolons

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const spdy = require('spdy'),
     express = require('express')     // path = require('path'),
 
  // Create Express Application
-const app = express();
-
+const app = express()
+const PORT = 3000
 // Require the routes
 app.use(require('./app/routes'))
 app.use(require('./app/routes/critical-foft-push'))
 app.use(require('./app/routes/critical-foft-push-variable-font'))
+app.use(require('./app/routes/critical-foft-preload'))
 app.use(require('./app/routes/critical-foft-preload-header'))
 app.use(require('./app/routes/no-font-loading-strategy'))
 
@@ -19,18 +20,18 @@ const options = {
         //ca: fs.readFileSync('keys/server.csr')
          // **optional** SPDY-specific options
   spdy: {
-    ssl: false
+    ssl: false  // TODO -should this be here??!
   }
 };
 
 // Assets and public folder - TODO describe static
-app.use(express.static('public'));
-app.use(express.static('app/assets'));
+app.use(express.static('public'))
+app.use(express.static('app/assets'))
 
 // Setup Server
-const server = spdy.createServer(options, app);   // TODO - how does spdy know it is HTTP/2?
+const server = spdy.createServer(options, app)   // TODO - how does spdy know it is HTTP/2?
 
-server.listen(3000, () => {
-    console.log(`Server started on port ${server.address().port}`);
+server.listen(PORT, () => {
+    console.log(`Server started on port ${server.address().port}`)
 });
 
