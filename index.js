ea0391a4031f19ce4dd23c78c095ff32c7ab886b
Mia Nygren <marianygren88@gmail.com>
2018-11-12 16:31:26 +0100

    Added pug template engine to express, and updated routes

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 // Require Modules
 const spdy = require('spdy'),
     fs = require('fs'),
-    express = require('express')     // path = require('path'),
+    express = require('express'),
+    path = require('path')
 
  // Create Express Application
 const app = express()
 const PORT = 3000
 
+// Set the template engine to Pug
+app.set('view engine','pug' )
+app.set('views', path.join(__dirname, './app/views'))
+
 // Require the routes
-app.use(require('./app/routes'))
+app.use(require('./server/routes'))
 
 // Routes for Critical FOFT Server Push
-app.use(require('./app/routes/critical-foft-push'))
-app.use(require('./app/routes/critical-foft-push-and-preload'))
+app.use(require('./server/routes/critical-foft-push'))
+app.use(require('./server/routes/critical-foft-push-and-preload'))
 
 // Routes for Critical FOFT preload (including variant1, variant2 and variant3)
-app.use(require('./app/routes/critical-foft-preload'))
+app.use(require('./server/routes/critical-foft-preload'))
 
 // Route for no strategy
-app.use(require('./app/routes/no-font-loading-strategy'))
+app.use(require('./server/routes/no-font-loading-strategy'))
 
 const options = {
-    key: fs.readFileSync('./app/keys/key.pem'),
-    cert: fs.readFileSync('./app/keys/cert.pem'),
+    key: fs.readFileSync('./server/keys/key.pem'),
+    cert: fs.readFileSync('./server/keys/cert.pem')
         //ca: fs.readFileSync('keys/server.csr')
          // **optional** SPDY-specific options
-  spdy: {
-    ssl: false  // TODO -should this be here??!
-  }
 };
 
 // Assets and public folder - TODO describe static
-app.use(express.static('public'))
+app.use(express.static('app/vendor'))
 app.use(express.static('app/assets'))
 
 // Setup Server
