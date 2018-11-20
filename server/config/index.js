1cb9440bbe01f3f6ab1a1e56f25b0313298278ef
Mia Nygren <marianygren88@gmail.com>
2018-11-20 19:30:38 +0100

    Added config for environments

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  development: {
+    siteName: 'Loading strategies for custom fonts [Development]'
+  },
+  production: {
+    siteName: 'Loading strategies for custom fonts'
+  }
+}
