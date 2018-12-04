b20d0d2b3a65847ac0c8f8ca398d47860efc22b9
Mia Nygren <marianygren88@gmail.com>
2018-12-04 15:26:17 +0100

    Added template variable page

--- server/routes/no-font-loading-strategy.js
+++ server/routes/no-font-loading-strategy.js
@@ -2,7 +2,9 @@ const express = require('express'),
     router = express.Router()
 
 router.get('/no-font-loading-strategy', function(req, res) {
-  res.render('no-font-loading-strategy')
+  res.render('no-font-loading-strategy', {
+    page: 'No Font Loading Strategy'
+    })
 });
 
 module.exports = router
