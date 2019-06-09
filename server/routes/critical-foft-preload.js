00404c9ba733fe46c3531c57df742116f142ef2a
Mia Nygren <marianygren88@gmail.com>
2019-06-10 00:36:10 +0200

    Added path to the response object

--- server/routes/critical-foft-preload.js
+++ server/routes/critical-foft-preload.js
@@ -5,13 +5,15 @@ const express = require('express'),
 
 router.get('/critical-foft-preload-variant1', function(req, res) {
   res.render('critical-foft-preload-variant1',{
-     page: 'Critical FOFT Preload (Variant1)'
+     page: 'Critical FOFT Preload (Variant1)',
+     path: req.path
   })
 });
 
 router.get('/critical-foft-preload-variant2', function(req, res) {
   res.render('critical-foft-preload-variant2', {
-    page: 'Critical FOFT Preload (Variant 2)'
+    page: 'Critical FOFT Preload (Variant 2)',
+    path: req.path
   })
 });
 
