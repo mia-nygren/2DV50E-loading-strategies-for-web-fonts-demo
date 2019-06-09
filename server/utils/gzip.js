ef25a24697c36105915831a8987e9aabb71ffcee
Mia Nygren <marianygren88@gmail.com>
2019-06-10 00:25:24 +0200

    Added gzip task that runs on server restart

---
+++ server/utils/gzip.js
@@ -0,0 +1,31 @@
+const zlib = require('zlib'),
+      fs = require('fs'),
+      path = require('path'),
+      { pipeline } = require('stream');
+
+const mainCSS = path.join(__dirname, '../../app/assets/styles/main.css')
+const prismCSS = path.join(__dirname, '../../app/assets/vendor/prism/prism.css')
+const bulmaCSS = path.join(__dirname, '../../app/assets/vendor/bulma/bulma.css')
+
+const gzipFile = (filePath => {
+  pipeline(
+    fs.createReadStream(filePath),
+    zlib.createGzip(),
+    fs.createWriteStream(filePath + '.gz'),
+      (err) => {
+        if (err) {
+          console.error('Pipeline failed.', err);
+        } else {
+          console.log('Pipeline succeeded.');
+        }
+    }
+  );
+}
+)
+
+// Gzip main styles (used for pushing) before serving
+gzipFile(mainCSS)
+gzipFile(prismCSS)
+gzipFile(bulmaCSS)
+
+console.log('Gzip complete');
